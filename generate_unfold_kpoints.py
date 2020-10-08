533a1ba74bd228567564daba39467a690dd530eb
CMUShuyang <shuyang1@andrew.cmu.edu>
2020-10-08 15:40:45 -0400

    Update the scripts

---
+++ generate_unfold_kpoints.py
@@ -0,0 +1,20 @@
+from unfold import make_kpath, removeDuplicateKpoints, find_K_from_k, save2VaspKPOINTS
+import numpy as np
+
+ # The tranformation matrix between supercell and primitive cell.
+M = [[ 0.,  1., -1.,],[ 1., -1.,  0.],[-9., -9., -9.]]
+# high-symmetry point of a Hexagonal BZ in fractional coordinate
+kpts = [[1, 0.5, 0.5],            # X
+        [0.0, 0.0, 0],            # G
+        [1, 0.5, 0.5]]            # X
+# create band path from the high-symmetry points, 30 points inbetween each pair of high-symmetry points
+kpath = make_kpath(kpts, nseg=30)
+K_in_sup = []
+for kk in kpath:
+    kg, g = find_K_from_k(kk, M)
+    K_in_sup.append(kg)
+# remove the duplicate K-points
+reducedK = removeDuplicateKpoints(K_in_sup)
+
+# save to VASP KPOINTS
+save2VaspKPOINTS(reducedK)
---
+++ unfold_plot.py
@@ -0,0 +1,22 @@
+from unfold import unfold, make_kpath
+import numpy as np
+import os
+
+# basis vector of the primitive cell
+fermi = os.popen('grep fermi OUTCAR').read().split()[14]
+M = [[ 0.,  1., -1.,],[ 1., -1.,  0.],[-9., -9., -9.]]
+WaveSuper = unfold(M=M, wavecar='WAVECAR', lsorbit=True)
+cell = [[0, 3.03,3.03],
+      [3.03, 0, 3.03],
+      [3.03, 3.03, 0]]
+kpts = [[1, 0.5, 0.5],            # X
+        [0.0, 0.0, 0],            # G
+        [1, 0.5, 0.5]]            # X
+kpath = make_kpath(kpts, nseg=30)
+from unfold import EBS_scatter
+sw = WaveSuper.spectral_weight(kpath)
+# show the effective band structure with scatter
+EBS_scatter(kpath, cell, sw, nseg=30, eref=float(fermi),
+                ylim=(-5, 0),
+                factor=100,
+                color = 'C0')
